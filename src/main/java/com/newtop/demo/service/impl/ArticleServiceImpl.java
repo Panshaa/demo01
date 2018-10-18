f7db1a0f0d31fb0ac4d209097444f301398b997d
彭丹略 <553987775@qq.com>
2018-10-18 17:48:51 +0800

    添加lsf4j日志控制

--- src/main/java/com/newtop/demo/service/impl/ArticleServiceImpl.java
+++ src/main/java/com/newtop/demo/service/impl/ArticleServiceImpl.java
@@ -6,6 +6,8 @@ import java.util.Date;
 import java.util.List;
 import java.util.UUID;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
@@ -26,6 +28,8 @@ import com.newtop.demo.vo.CategoryVO;
 @Service
 @Transactional
 public class ArticleServiceImpl implements ArticleService {
+
+	private static Logger log = LoggerFactory.getLogger(ArticleServiceImpl.class);
 
 	@Autowired
 	private JdbcTemplate jdbcTemplate;
@@ -49,7 +53,6 @@ public class ArticleServiceImpl implements ArticleService {
 				vo.setCreateTime(rs.getTimestamp("createTime"));
 				vo.setStatu(rs.getInt("statu"));
 				vo.setIsDeleted(rs.getInt("isDeleted") == 1 ? true : false);
-
 				// 根据用户ID查找文章作者
 				String sql = "select * from user where uid = ?";
 				User user = jdbcTemplate.query(sql, new RowMapper<User>() {
@@ -68,7 +71,10 @@ public class ArticleServiceImpl implements ArticleService {
 		}, id);
 
 		// 判断是否存在这个ID对应的文章，不存在则抛出自定义异常
-		if(list==null||list.size()<1) throw new ArticleException("不存在此文章，查找失败");
+		if (list == null || list.size() < 1) {
+			log.info("--------查找文章失败--------");
+			throw new ArticleException("不存在此文章，查找失败");
+		}
 
 		// 在中间表查找文章分类信息
 		String sql2 = "select c.* from category_article ca,category c,article a where a.aid = ca.aid and c.cid = ca.cid and ca.aid = ?";
@@ -89,6 +95,8 @@ public class ArticleServiceImpl implements ArticleService {
 		}, id);
 
 		list.get(0).setCategories(list2);
+
+		log.info("--------查找文章成功--------");
 
 		return list.get(0);
 	}
@@ -129,17 +137,26 @@ public class ArticleServiceImpl implements ArticleService {
 		// 使用uuid为文章添加主键id
 		String uuid = UUID.randomUUID().toString().replace("-", "");
 		String sql = "insert into article values(?,?,?,?,?,?,0)";
-		int i = jdbcTemplate.update(sql, uuid, bo.getArticleTitle(), bo.getArticleBody(), new Date(), bo.getCreateId(), bo.getStatu());
-		
-		if(i<1) throw new ArticleException("文章保存失败！"); 
+		int i = jdbcTemplate.update(sql, uuid, bo.getArticleTitle(), bo.getArticleBody(), new Date(), bo.getCreateId(),
+				bo.getStatu());
+
+		if (i < 1) {
+			log.info("--------新添文章失败--------");
+			throw new ArticleException("文章保存失败！");
+		}
 
 		// 为文章添加分类
 		for (String cid : bo.getCids()) {
 			String c_a_uuid = UUID.randomUUID().toString().replace("-", "");
 			String c_a_sql = "insert into category_article values(?,?,?)";
 			i = jdbcTemplate.update(c_a_sql, c_a_uuid, uuid, cid);
-			if(i<1) throw new ArticleException("文章保存失败！"); 
+			if (i < 1) {
+				log.info("--------文章添加分类失败--------");
+				throw new ArticleException("文章保存失败！");
+			}
 		}
+
+		log.info("--------新添文章成功--------");
 	}
 
 	/**
@@ -149,19 +166,30 @@ public class ArticleServiceImpl implements ArticleService {
 	public void updateArticle(ArticleBO bo) {
 		String sql1 = "update article set articleTitle = ?,articleBody = ? where aid = ?";
 		int i = jdbcTemplate.update(sql1, bo.getArticleTitle(), bo.getArticleBody(), bo.getAid());
-		if(i<1) throw new ArticleException("文章更新失败！");
+		if (i < 1) {
+			log.info("--------文章更新失败--------");
+			throw new ArticleException("文章更新失败！");
+		}
 		// 删除原来的分类
 		String sql2 = "delete from category_article where aid = ?";
 		i = jdbcTemplate.update(sql2, bo.getAid());
-		if(i<1) throw new ArticleException("文章更新失败！");
-		
+		if (i < 1) {
+			log.info("--------更新文章删除原先分类失败--------");
+			throw new ArticleException("文章更新失败！");
+		}
+
 		// 为文章添加改变更新后的分类
 		for (String cid : bo.getCids()) {
 			String c_a_uuid = UUID.randomUUID().toString().replace("-", "");
 			String c_a_sql = "insert into category_article values(?,?,?)";
 			i = jdbcTemplate.update(c_a_sql, c_a_uuid, bo.getAid(), cid);
-			if(i<1) throw new ArticleException("文章更新失败！");
+			if (i < 1) {
+				log.info("--------更新文章重新添加分类失败--------");
+				throw new ArticleException("文章更新失败！");
+			}
 		}
+
+		log.info("--------文章更新成功--------");
 	}
 
 	/**
@@ -171,10 +199,17 @@ public class ArticleServiceImpl implements ArticleService {
 	public void deleteArticleById(String id) {
 		String sql1 = "delete from category_article where aid = ?";
 		int i = jdbcTemplate.update(sql1, id);
-		if(i<1) throw new ArticleException("文章删除失败！");
+		if (i < 1) {
+			log.info("--------删除文章分类失败--------");
+			throw new ArticleException("文章删除失败！");
+		}
 		String sql2 = "delete from article where aid = ?";
 		i = jdbcTemplate.update(sql2, id);
-		if(i<1) throw new ArticleException("文章删除失败！");
+		if (i < 1) {
+			log.info("--------删除文章失败--------");
+			throw new ArticleException("文章删除失败！");
+		}
+		log.info("--------文章删除成功--------");
 	}
 
 }
